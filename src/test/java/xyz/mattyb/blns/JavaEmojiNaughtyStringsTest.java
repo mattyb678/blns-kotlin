0e3d147e98cbe5098475a4a95de2eb93f8e198b8
Matt Berteaux <mattyb678@gmail.com>
2020-05-26 22:42:37 -0600

    Get rid of java file with main method, use java test class

---
+++ src/test/java/xyz/mattyb/blns/JavaEmojiNaughtyStringsTest.java
@@ -0,0 +1,17 @@
+package xyz.mattyb.blns;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JavaEmojiNaughtyStringsTest {
+
+    @Test
+    void testHearts() {
+        assertThat(NaughtyStrings.Emoji.Hearts.all()).hasSize(15);
+        assertThat(NaughtyStrings.Emoji.Hearts.drop(9).all()).hasSize(6);
+        assertThat(NaughtyStrings.Emoji.Hearts.drop(2).drop(9).take(2))
+                .containsExactly("\uD83D\uDC9C", "\uD83D\uDC9B");
+    }
+
+}
